ba73231f43c0cc781071ea2dbddacf21715e5d3b
tranquocviet-dev <trquocviet2k7@gmail.com>
2026-06-18 07:56:23 +0700

    symlink

--- hjem.nix
+++ hjem.nix
@@ -5,47 +5,15 @@
       user = "dice"; # this is the name of the user
       directory = "/home/dice"; # where the user's $HOME resides
       files = {
-        ".config/helix/config.toml".text = ''
-          theme = "noctalia"
-          [editor]
-          line-number = "relative"
-          [editor.file-picker]
-          hidden = true
-          [editor.whitespace.render]
-          space = "all"
-          tab = "all"
-          newline = "none"
-        '';
-        ".config/helix/languages.toml".text = ''
-            [language-server.ccls]
-            command = "ccls"
-
-            [[language]]
-            name = "c"
-            language-servers = ["ccls"]
-
-            [language-server.superhtml]
-            command = "superhtml"
-
-            [[language]]
-            name = "html"
-            language-servers = ["superhtml"]
-            [language-server.pyright]
-            command = "pyright"
-
-            [[language]]
-            name = "python"
-            language-servers = [ "pyright" ]
-
-
-            [language-server.nixd]
-            command = "nixd"
-
-            [[language]]
-            name = "nix"
-            language-servers = [ "nixd" ]
-          '';
-     };
+        ".config/helix/config.toml" = {
+          source = ./symlinkfiles/helix/config.toml;
+          type = "symlink";
+        };
+         ".config/helix/languages.toml" = {
+           source = ./symlinkfiles/helix/languages.toml;
+           type = "symlink";
+         };
+      };
     };
   };
 }
