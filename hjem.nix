8e10d581162083c0793b39f7ef099efd3fc29755
tranquocviet-dev <trquocviet2k7@gmail.com>
2026-06-18 23:25:13 +0700

    niri declaratively and vicinae instead of noctalia launcher

--- hjem.nix
+++ hjem.nix
@@ -5,6 +5,10 @@
       user = "dice"; # this is the name of the user
       directory = "/home/dice"; # where the user's $HOME resides
       files = {
+        ".config/niri/v5.kdl" = {
+          source = ./symlinkfiles/niri/v5.kdl;
+          type = "symlink";
+        };
         ".config/helix/config.toml" = {
           source = ./symlinkfiles/helix/config.toml;
           type = "symlink";
