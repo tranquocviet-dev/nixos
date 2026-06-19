ed98e8a76463121ad2e37245cccd801dc2e3b5db
tranquocviet-dev <trquocviet2k7@gmail.com>
2026-06-19 10:21:36 +0700

    replacing hjem with full hm setup

--- hjem.nix
+++
@@ -1,9 +0,0 @@
-{
-  hjem.users = {
-    dice = {
-      # enable = true; # This is not necessary, since enable is 'true' by default
-      user = "dice"; # this is the name of the user
-      directory = "/home/dice"; # where the user's $HOME resides
-    };
-  };
-}
