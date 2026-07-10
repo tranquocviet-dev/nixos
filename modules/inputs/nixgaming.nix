ea8c5bec527cf307047bc6595057147102a76d65
tranquocviet-dev <trquocviet2k7@gmail.com>
2026-07-10 17:43:16 +0700

    osu lazer

--- modules/inputs/nixgaming.nix
+++ modules/inputs/nixgaming.nix
@@ -2,5 +2,6 @@
 {
   environment.systemPackages = [ # or home.packages
     inputs.nix-gaming.packages.${pkgs.stdenv.hostPlatform.system}.wine-tkg
+    inputs.nix-gaming.packages.${pkgs.stdenv.hostPlatform.system}.osu-lazer-bin
   ];
 }
--- modules/inputs/osu.nix
+++ modules/inputs/osu.nix
@@ -3,6 +3,5 @@
   environment.systemPackages = [
     # Install the package
     inputs.osu-stable.packages.x86_64-linux.default
-    inputs.osu-lazer-bin.packages.x86_64-linux.default
   ];
 }
