6d843f8dc9e0379c87152fcfebcb2500822e4315
tranquocviet-dev <trquocviet2k7@gmail.com>
2026-07-12 01:59:23 +0700

    fonts

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -4,15 +4,9 @@
   fonts.fontDir.enable = true;
   fonts.packages = with pkgs; [
     roboto
-    nerd-fonts.iosevka
     nerd-fonts.iosevka-term-slab
-    nerd-fonts.jetbrains-mono
-    nerd-fonts.comic-shanns-mono
     nerd-fonts.roboto-mono
-    sarasa-gothic
     noto-fonts-cjk-sans
     noto-fonts-cjk-serif
-    maple-mono.NF-CN
-    maple-mono.NF
   ];
 }
