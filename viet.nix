4fcd3144e1ce9dd5cd8cf07bda5c10b633f93515
tranquocviet-dev <trquocviet2k7@gmail.com>
2026-07-11 21:18:48 +0700

    user optimizations

---
+++ viet.nix
@@ -0,0 +1,64 @@
+{ system, inputs, pkgs, config, user, ... }: {
+  home.username = "${user}";
+  home.homeDirectory = "/home/${user}";
+  home.stateVersion = "26.05";
+  # 1. Importing my nix files
+  home.packages = [
+    pkgs.vicinae
+    pkgs.devenv
+    pkgs.adwaita-icon-theme
+    pkgs.papirus-icon-theme
+  ];
+  # 2. Configure GTK
+  gtk = {
+    enable = true;
+    font.name = "RobotoMono Nerd Font Propo";
+    font.size = 16;
+    # 3. Set the theme name (exact name of the theme folder)
+    theme = {
+      name = "adw-gtk3-dark";
+      package = pkgs.adw-gtk3;
+    };
+    
+    # Optional: Set your icons and cursors
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+  };
+  programs.vicinae = {
+    enable = true;
+    systemd = {
+      enable = true;
+      autoStart = true;
+    };
+  };
+  home.file = {
+    ".config/niri" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixos/symlinkfiles/niri";
+      force = true;
+    };
+    ".config/mango" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixos/symlinkfiles/mango";
+      force = true;
+    };
+    ".config/helix" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixos/symlinkfiles/helix";
+    };
+    ".config/emacs/init.el" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixos/symlinkfiles/emacs/init.el";
+    };
+    ".config/starship.toml" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixos/symlinkfiles/starship.toml";
+    };
+    ".config/emacs/autoload" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixos/symlinkfiles/emacs/autoload";
+    };
+    ".config/emacs/themes" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixos/symlinkfiles/emacs/themes";
+    };
+    ".config/fastfetch" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixos/symlinkfiles/fastfetch";
+    };
+  };
+}
