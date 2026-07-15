6709951579fd4d0336e3848e6231c2988d1f178b
tranquocviet-dev <trquocviet2k7@gmail.com>
2026-07-16 00:06:33 +0700

    cachix

--- modules/nh.nix
+++ modules/nh.nix
@@ -5,7 +5,7 @@
     enable = true;
     clean = {
       enable = true;
-      extraArgs = "--keep-since 10d --keep 20";
+      extraArgs = "--keep-since 10d --keep 10";
     };
     flake = "/home/${user}/.config/nixos"; # Replace with the absolute path to your config
   };
