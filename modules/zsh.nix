5f91b5f7a6c9a2773944d69f57e4cf220ae16cb2
tranquocviet-dev <trquocviet2k7@gmail.com>
2026-07-23 16:55:44 +0700

    fish to zsh

---
+++ modules/zsh.nix
@@ -0,0 +1,14 @@
+{ self, pkgs, specialArgs, ... }:
+{
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      ls = "eza";
+    };
+  };
+  # Set fish as the default shell for your user
+  users.users.${specialArgs.user} = {
+    shell = pkgs.zsh;
+  };
+  programs.starship.enable = true;
+}
