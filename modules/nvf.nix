19759004b9c80d5568a886502de8e8d7513c971d
tranquocviet-dev <trquocviet2k7@gmail.com>
2026-07-24 15:30:13 +0700

    nvf telescope

--- modules/nvf.nix
+++ modules/nvf.nix
@@ -8,7 +8,7 @@
                 nvim-web-devicons.enable = true;
             };
             options = {
-				winborder = "single";
+				winborder = "rounded";
                 completeopt = ["menu" "menuone" "noselect" "noinsert"];
                 signcolumn = "no";
                 expandtab = false;
