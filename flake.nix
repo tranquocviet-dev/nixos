c5f8f649468341c7eb6780a9964974de72f81a8f
tranquocviet-dev <trquocviet2k7@gmail.com>
2026-08-11 19:10:17 +0700

    tack

--- flake.nix
+++ flake.nix
@@ -1,31 +1,18 @@
 {
 	description = "NixOS configuration with Noctalia";
 
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+	outputs = { self, ... }@args:
+	let
+		inputs = (import ./.tack) { overrides = args.tackOverrides or { }; };
 
-		noctalia-greeter.url = "github:noctalia-dev/noctalia-greeter";
-
-		home-manager = {
-			url = "github:nix-community/home-manager";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-
-		noctalia.url = "github:noctalia-dev/noctalia/cachix";
-
-		osu-stable.url = "path:./pkgs/osu-stable";
-		osu-lazer-bin.url = "path:./pkgs/osu-lazer-bin";
-
-		nvf.url = "github:notashelf/nvf";
-	};
-
-	outputs = { self, nixpkgs, home-manager, nvf, ... }@inputs: {
+		inherit (inputs) nixpkgs home-manager nvf;
+	in {
 		nixosConfigurations = let
 			mkSystem = hostname:
-			{
-				system ? "x86_64-linux",
-				user ? "dice",
-			}:
+				{
+					system ? "x86_64-linux",
+					user ? "dice",
+				}:
 				nixpkgs.lib.nixosSystem {
 					system = system;
 					modules = let
