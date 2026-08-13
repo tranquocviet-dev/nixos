97998b4e1c6775748cc6f6d7d74685c16fbba5da
tranquocviet-dev <trquocviet2k7@gmail.com>
2026-08-13 09:45:10 +0700

    commenting flake.nix and update

--- flake.nix
+++ flake.nix
@@ -1,15 +1,20 @@
 {
 	description = "NixOS configuration with Noctalia";
 	outputs = args @ { self, ... }: let
+		# tack inputs
 		inputs = import ./.tack {
 			overrides = args.tackOverrides or {};
 		};
-		inherit (inputs.nixpkgs) lib;
+		inherit (inputs.nixpkgs) lib; # to use for recursiveImport below
 		recursiveImport = import ./lib/recursive_import.nix { inherit lib; };
 		specialArgs = {
+			# passing inputs and self for general use
+			# passing down recursiveImport in case a file wants that
 			inherit inputs self recursiveImport;
 		};
 		commonModules = [
+			# eljanfus' approach to passing down common modules
+			# not used much by me but looks useful so ill leave them there
 			{
 				home-manager.extraSpecialArgs = {inherit recursiveImport;};
 				home-manager.sharedModules = [inputs.nvf.homeManagerModules.default];
--- modules/fonts.nix
+++ modules/fonts.nix
@@ -3,7 +3,6 @@
 	fonts.enableDefaultPackages = true;
 	fonts.fontDir.enable = true;
 	fonts.packages = with pkgs; [
-		noto-fonts-cjk-sans
 		nerd-fonts.noto
 	];
 }
