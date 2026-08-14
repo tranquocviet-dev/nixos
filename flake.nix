f6f4ba5a7f99e41588008931abebdb85937b7b0f
tranquocviet-dev <trquocviet2k7@gmail.com>
2026-08-14 09:46:00 +0700

    deleting old modules

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 	outputs = args @ { self, ... }: let
 		# tack inputs
 		inputs = import ./.tack {
-			overrides = args.tackOverrides or {};
+			overrides = args.tackOverrides or { };
 		};
 		# 2. Instantiate recursiveImport using nixpkgs lib
 		recursiveImport = import ./lib/recursive_import.nix {
@@ -11,19 +11,11 @@
 		};
 	in inputs.flake-parts.lib.mkFlake ( args // { inherit inputs; } ) {
 		systems = [ "x86_64-linux" ];
-		
 		imports = [
+			# even though theres only 1 entry i leave this on a new line for easier time adding more hosts later
 			./host/nixos
-		] ++ recursiveImport [
-			./modules
-		];
-		
-		flake = {
-			
-		};
-		
-		perSystem = {
-			
-		};
+		] ++ recursiveImport [ ./modules ];
+		flake = { };
+		perSystem = { };
 	};
 }
