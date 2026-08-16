51dc9e50bc2c1cf38d228ff99e62eabeef97fd59
tranquocviet-dev <trquocviet2k7@gmail.com>
2026-08-17 01:49:36 +0700

    removing noctalia-greeter input in favour of nixpkgs noctalia-greeter

--- modules/inputs/_noctalia-greeter.nix
+++
@@ -1,8 +0,0 @@
-{ ... }: {
-	flake.nixosModules.inputs-noctalia-greeter = { inputs, ... }: {
-		imports = [
-			inputs.noctalia-greeter.nixosModules.default
-		];
-		programs.noctalia-greeter.enable = true;
-	};
-}
