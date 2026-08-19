e6b778819689e1c73ffb79505c6cd6dfed50c8f7
tranquocviet-dev <trquocviet2k7@gmail.com>
2026-08-19 15:09:30 +0700

    tried to add npins, failed

--- flake.nix
+++ flake.nix
@@ -2,22 +2,20 @@
 	description = "NixOS configuration with Noctalia";
 	outputs = args @ { self, ... }: let
 		# tack inputs
-		#inputs = import ./.tack {
-		#	overrides = args.tackOverrides or { };
-		#};
-		# npins inputs
-		sources = import ./npins;
-		inputs = sources // {
-			osu-stable = ./pkgs/osu-stable;
-			osu-lazer-bin = ./pkgs/osu-lazer-bin;
+		inputs = import ./.tack {
+			overrides = args.tackOverrides or { };
 		};
+		# npins inputs
+		#sources = import ./npins;
+		#inputs = sources // {
+		#	osu-stable = ./pkgs/osu-stable;
+		#	osu-lazer-bin = ./pkgs/osu-lazer-bin;
+		#};
 		# 2. Instantiate recursiveImport using nixpkgs lib
 		recursiveImport = import ./lib/recursive_import.nix {
 			inherit (inputs.nixpkgs) lib;
 		};
-		flake-parts-lib = (import inputs.flake-parts { inherit (inputs.nixpkgs) lib; }).lib;
-	#in inputs.flake-parts.lib.mkFlake ( args // { inherit inputs; } ) {
-	in flake-parts-lib.mkFlake ( args // { inherit inputs; } ) {
+	in inputs.flake-parts.lib.mkFlake ( args // { inherit inputs; } ) {
 		systems = [ "x86_64-linux" ];
 		imports = [
 			# even though theres only 1 entry i leave this on a new line for easier time adding more hosts later
