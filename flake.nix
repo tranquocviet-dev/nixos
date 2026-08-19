cd83dbc3fec00e2128a80c6fb0b6169cdc09426a
tranquocviet-dev <trquocviet2k7@gmail.com>
2026-08-19 15:07:12 +0700

    npins

--- flake.nix
+++ flake.nix
@@ -2,14 +2,22 @@
 	description = "NixOS configuration with Noctalia";
 	outputs = args @ { self, ... }: let
 		# tack inputs
-		inputs = import ./.tack {
-			overrides = args.tackOverrides or { };
+		#inputs = import ./.tack {
+		#	overrides = args.tackOverrides or { };
+		#};
+		# npins inputs
+		sources = import ./npins;
+		inputs = sources // {
+			osu-stable = ./pkgs/osu-stable;
+			osu-lazer-bin = ./pkgs/osu-lazer-bin;
 		};
 		# 2. Instantiate recursiveImport using nixpkgs lib
 		recursiveImport = import ./lib/recursive_import.nix {
 			inherit (inputs.nixpkgs) lib;
 		};
-	in inputs.flake-parts.lib.mkFlake ( args // { inherit inputs; } ) {
+		flake-parts-lib = (import inputs.flake-parts { inherit (inputs.nixpkgs) lib; }).lib;
+	#in inputs.flake-parts.lib.mkFlake ( args // { inherit inputs; } ) {
+	in flake-parts-lib.mkFlake ( args // { inherit inputs; } ) {
 		systems = [ "x86_64-linux" ];
 		imports = [
 			# even though theres only 1 entry i leave this on a new line for easier time adding more hosts later
