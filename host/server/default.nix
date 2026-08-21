efbecef484e442c90f41a40fbb24457cd5376780
tranquocviet-dev <trquocviet2k7@gmail.com>
2026-08-21 21:14:10 +0700

    create user for server

---
+++ host/server/default.nix
@@ -0,0 +1,20 @@
+{ inputs, self, ... }: let
+	system = "x86_64-linux";
+	user = "dice";
+in {
+	flake.nixosConfigurations.nixos = inputs.nixpkgs.lib.nixosSystem {
+		specialArgs = {
+			inherit inputs self system user;
+		};
+		modules = [
+			# 1. Host-specific hardware / config
+			./configuration.nix
+			./hardware-configuration.nix
+			# Host settings
+			({ ... }: {
+				nixpkgs.hostPlatform = "x86_64-linux";
+				networking.hostName = "nixos";
+			})
+		];
+	};
+}
